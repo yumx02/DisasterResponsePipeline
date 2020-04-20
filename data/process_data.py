105e9514cc3ea2ef7d5d5b9287d769f59130e827
yumx02 <yamamoto.yuka.72@gmail.com>
2020-04-20 12:00:39 +0900

    write docstring

--- data/process_data.py
+++ data/process_data.py
@@ -3,17 +3,40 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    '''load data
+    INPUT:
+    messages_filepath - messages csv file path
+    categories_filepath - categories csv file path
+
+    OUTPUT:
+    messages - pd.dataframe with id, message, original, genre columns
+    categories - pd.dataframe with id, categories columns
+
+    Description:
+    load csv file and store it with pd dataframes
+    '''
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
-    return messages, categories
+
+    df = messages.merge(categories, how= 'outer', on =['id'])
+
+    return df
 
 def clean_data(df):
-    messages, categories = load_data(sys.argv[1],sys.argv[2])
-    df = messages.merge(categories, how= 'outer', on =['id'])    # merge datasets
+    '''clean data
+    INPUT:
+    df - pd.dataframe with id, message, original, genre, categories columns
+
+    OUTPUT:
+    df - pd.dataframe with id, message, original, genre,
+         and 36 categories(related, request...etc) columns
+
+    Description:
+    create 36 individual category column with 0 or 1
+    '''
     categories = df['categories'].str.split(';',expand=True)    # create 36 individual category column
     row = categories.loc[0]     # select the first row of the categories dataframe
     category_colnames = row.str[:-2] # extract a list of new column names for categories.
-
     categories.columns = category_colnames    # rename the columns
     for column in categories:
         categories[column] = categories[column].str[-1] # set each value to be the last character of the string
@@ -25,8 +48,15 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
-    df = clean_data(df)
+    '''save data
+    INPUT:
+    df - pd.dataframe with id, message, original, genre,
+         and 36 categories(related, request...etc) columns
+    database_filename - database file path and name
 
+    Description:
+    create 36 individual category column with 0 or 1
+    '''
     engine = create_engine('sqlite:///data/DisasterResponse.db')
     df.to_sql('data_disaster', engine, index=False)
 
